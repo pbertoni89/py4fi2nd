9c5bd3aae28be79adcbe745653039b474c3b9763
Patrizio Bertoni <patrizio.bertoni.89@gmail.com>
2026-02-28 21:47:44 +0100

    nbstripping out + started ch13/13_a_statistics

--- code/dx/get_year_deltas.py
+++ code/dx/get_year_deltas.py
@@ -14,7 +14,6 @@ import numpy as np
 def get_year_deltas(date_list, day_count=365.):
     ''' Return vector of floats with day deltas in year fractions.
     Initial value normalized to zero.
-
     Parameters
     ==========
     date_list: list or array
@@ -22,13 +21,11 @@ def get_year_deltas(date_list, day_count=365.):
     day_count: float
         number of days for a year
         (to account for different conventions)
-
     Results
     =======
     delta_list: array
         year fractions
     '''
-
     start = min(date_list)
     delta_list = [(date - start).days / day_count
                   for date in date_list]
